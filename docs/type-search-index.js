df60c1fe837c5bb379a12df7612cac8c09a5c248
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2022-04-13 19:41:04 +0200

    Javadocs and Copyright update

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"me.itoncek.popliktroll","l":"Events"},{"p":"me.itoncek.popliktroll","l":"PoplikTroll"}];updateSearchResults();
