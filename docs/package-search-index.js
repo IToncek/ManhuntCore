814d3ab76bfeb51ca9e25d6fb16dff14f62b0cb9
Made By IToncek <toncek2019@post.cz>
2022-04-15 10:40:02 +0000

    Maven artifacts for 1.0-SNAPSHOT

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.itoncek.manhuntcore"},{"l":"me.itoncek.manhuntcore.commands"},{"l":"me.itoncek.manhuntcore.util"}];updateSearchResults();
